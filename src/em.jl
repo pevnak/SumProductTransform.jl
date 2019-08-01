fb5d8293fe5a55d201a664c62cc6bdf6761915e1
Pevnak <pevnak@gmail.com>
2019-08-01 13:25:37 +0200

    transition

--- src/em.jl
+++ src/em.jl
@@ -1,3 +1,5 @@
+using Zygote: dropgrad
+
 struct DenseMixture{T,C}
 	components::Vector{C}
 	prior::Vector{T}
@@ -17,15 +19,16 @@ Flux.mapchildren(f, x::DenseMixture) = f.(Flux.children(x))
 """
 function estep!(m::DenseMixture, x)
 	lkl = reduce(vcat, [transpose(logpdf(c, x)) for c in m.components])
-	y = mapslices(argmax, softmax(lkl), dims = 1)
-	o = Flux.onehotbatch(y[:], 1:length(m.components))
+	y = Flux.onecold(lkl)
+	o = Flux.onehotbatch(y, 1:length(m.components))
+	m.prior .= mean(o, dims = 2)
 	mean( o .* lkl)
 end
 
 function estep(m::DenseMixture, x)
 	lkl = hcat([logpdf(c, x) for c in m.components]...)
-	y = mapslices(argmax, lkl, dims = 2)
-	o = Flux.onehotbatch(y[:], 1:length(m.components))
+	y = Flux.onecold(transpose(lkl))
+	o = Flux.onehotbatch(dropgrad(y), 1:length(m.components))
 	mean( transpose(o) .* lkl)
 end
 
@@ -33,7 +36,7 @@ function Distributions.logpdf(m::DenseMixture, x::AbstractMatrix)
 	lkl = hcat([logpdf(c, x) for c in m.components]...)
 	# log.(sum(m.prior' .* exp.(lkl), dims = 2))
 	# log.(sum(m.prior' .* exp.(lkl), dims = 2))
-	logsumexp(m.prior' .+ lkl, dims = 2)
+	logsumexp(log.(m.prior' .+ 1f-8) .+ lkl, dims = 2)
 end
 
 Distributions.logpdf(m::DenseMixture, x::Tuple) = logpdf(m, reshape(collect(x), :, 1))[1]
--- test/play.jl
+++ test/play.jl
@@ -1,20 +1,63 @@
-using ToyProblems, Distributions, DenseMixtureModels, Unitary, Flux, FluxExtensions
+using ToyProblems, Distributions, DenseMixtureModels, Unitary, Flux
 using DenseMixtureModels: estep!, estep, DenseMixture, DenseP
 using Unitary: SVDDense
 using Plots
 plotly()
 
-x = eightskewedgaussians(200)
+function visualize(m, x)
+	xr = range(minimum(x[1,:]) - 1 , maximum(x[1,:])+ 1 , length = 100)
+	yr = range(minimum(x[2,:]) - 1 , maximum(x[2,:])+ 1 , length = 100)
+	contour(xr, yr, (x...) ->  Flux.data(logpdf(m, x)));
+	scatter!(x[1,:], x[2,:])
+end
 
-m = DenseMixture([DenseP(SVDDense(identity), MultivariateNormal(2,1)) for i in 1:8], fill(1/8, 8));
+function createmixture(n, p = () -> MultivariateNormal(2,1))
+	DenseMixture([DenseP(SVDDense(identity), p()) for i in 1:n], fill(1/n, n))
+end
+###############################################################################
+#			Let's do a single mixture
+###############################################################################
+x = eightskewedgaussians(200)
+m = createmixture(8);
 l = estep(m, x)
 ps = Flux.params(m)
 opt = ADAM()
 Flux.train!(i -> -estep(m, x), Flux.Params(ps), 1:10000, opt; cb = () -> @show estep(m, x))
 estep!(m, x)
+visualize(m, x)
 
-xr = range(minimum(x[1,:]) - 1 , maximum(x[1,:])+ 1 , length = 100)
-yr = range(minimum(x[2,:]) - 1 , maximum(x[2,:])+ 1 , length = 100)
-contour(xr, yr, (x...) ->  Flux.data(logpdf(m, x)));
-scatter!(x[1,:], x[2,:])
 
+
+###############################################################################
+#			Let's try two nested mixtures
+###############################################################################
+m = createmixture(4, () -> createmixture(2))
+l = estep(m, x)
+ps = Flux.params(m)
+opt = ADAM()
+Flux.train!(i -> -estep(m, x), Flux.Params(ps), 1:10000, opt; cb = () -> @show estep(m, x))
+estep!(m, x)
+visualize(m, x)
+
+###############################################################################
+#			Let's try shared inner mixture
+###############################################################################
+mi = createmixture(4);
+m = createmixture(4, () -> mi)
+ps = Flux.params(m)
+opt = RMSProp(0.01)
+Flux.train!(i -> -estep(m, x), Flux.Params(ps), 1:5000, opt; cb = () -> @show estep(m, x))
+estep!(m, x) # -0.15319616593710528 (tracked)
+visualize(m, x)
+
+###############################################################################
+#			Let's try shared more
+###############################################################################
+mmi = createmixture(3);
+mi = createmixture(3, () -> mmi);
+m = createmixture(3, () -> mi);
+ps = Flux.params(m)
+opt = RMSProp(0.01)
+Flux.train!(i -> -estep(m, x), Flux.Params(ps), 1:5000, opt; cb = () -> @show estep(m, x))
+estep!(m, x) 
+visualize(m, x)
