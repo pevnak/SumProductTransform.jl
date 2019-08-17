092708127a8cd0ae449a10815eabbb42d9b85c28
Pevnak <pevnak@gmail.com>
2019-08-17 17:47:51 +0200

    added a small test for sampling

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,2 +1,15 @@
+using SumDenseProduct, Test
 include("builder.jl")
 include("productnode.jl")
+
+
+@testset "sampling" begin
+	ns = [2,2]
+	σs = [identity, identity]
+	noise = [4,0]
+	d = 8
+	for s in [:all, :none, :dense]
+	    m = buildmixture(d, ns, σs, noise, sharing = s)
+	    @test length(rand(m)) == 8
+	end
+end
