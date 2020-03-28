1a54b19047df570a41d20ab119d70cbd81ffc7b4
Pevnak <pevnak@gmail.com>
2020-03-28 14:28:11 +0100

    small polish

--- examples/variational/vae.jl
+++ examples/variational/vae.jl
@@ -8,10 +8,9 @@ includet("distributions.jl")
 x = flower(Float32,200)
 K = 9
 components = tuple([DenseNode(Unitary.SVDDense(2, identity, :butterfly), MvNormal(2,1f0)) for _ in 1:K]...)
-ps = Flux.params(components)
-τ = 1f0
-
 q = Chain(Dense(2,10,relu), Dense(10,10,relu), Dense(10,9))
+
+ps = Flux.params(components)
 push!(ps, Flux.params(q))
 
 opt = ADAM()
@@ -25,6 +24,3 @@ for i in 1:10000
 		@show mean(softmax_log_likelihood(components, q(x), x))
 	end
 end
-
-model = SumNode([DenseNode(Unitary.SVDDense(2, identity, :butterfly), MvNormal(2,1f0)) for _ in 1:K])
-fit!(model, x, 100, 10000, 0; gradmethod = :exact, minimum_improvement = -1e10, opt = ADAM())
