d1224611708c5c259ee3fc1f970b9f19fbda37a6
mlnpapez <mlnpapez@gmail.com>
2020-12-02 18:13:30 +0100

    experiments: the normalizing constant (update)

--- examples/variational/normalizing_constant.jl
+++ examples/variational/normalizing_constant.jl
@@ -5,7 +5,8 @@ using SumProductTransform:
 	ScaleShift,
 	samplebatch,
 	logpdf,
-	maptree,
+	# maptree,
+	sampletree,
 	treelogpdf
 using DistributionsAD: TuringMvNormal
 using ToyProblems: flower2
@@ -27,25 +28,67 @@ function sptn(d, n, l)
     return(m)
 end
 
-function gd!(m, x, maxsteps::Int, batchsize::Int; opt = ADAM(), ps = params(m))
+function gd!(m::SumNode,
+			 x::Array{Float64, 2},
+			 maxsteps::Int;
+			 opt = ADAM(),
+			 ps = params(m))
+
+	for t in 1:maxsteps
+		gs = gradient(() -> -mean(logpdf(m, x)), ps)
+		Optimise.update!(opt, ps, gs)
+
+		mod(t, 1000) == 0 && @show mean(logpdf(m, x))
+	end
+end
+
+function gd!(m::SumNode,
+			 x::Array{Float64, 2},
+			 maxsteps::Int,
+			 batchsize::Int;
+			 opt = ADAM(),
+			 ps = params(m),
+			 normconst::Symbol = :exact)
+
 	for t in 1:maxsteps
 		batch = samplebatch(x, batchsize)
-		gs = gradient(() -> logpdf(m, batch[:,1]), ps)
+		gs = gradient(() -> logpdf(m, batch[:, 1]), ps)
+
 		foreach(p -> gs[p] !== nothing && gs[p] .= 0, ps)
 		foreach(1:batchsize) do i
             bi = batch[:, i]
-			tri = maptree(m, batch[:, i:i])[2][1]
-			onegs = gradient(() -> -mean(exp.(treelogpdf(m, bi, tri))), ps) 
-			val = mean(exp.(logpdf(m, bi)))
-			foreach(p -> gs[p] .= gs[p] == nothing ? onegs[p] ./ val : gs[p] .+ onegs[p] ./ val, filter(p -> !isnothing(onegs[p]), collect(ps)))
+			# ti = maptree(m, batch[:, i:i])[2][1]
+			ti = sampletree(m)
+
+			gi = gradient(() -> -mean(exp.(treelogpdf(m, bi, ti))), ps)
+			if normconst == :exact
+				ni = mean(exp.(logpdf(m, bi)))
+
+			elseif normconst == :sample
+				ni = mean(exp.(treelogpdf(m, bi, ti)))
+
+			else
+				@error "unknown setting"
+			end
+
+			foreach(p -> gs[p] .= gs[p] == nothing ? gi[p] ./ ni : gs[p] .+ gi[p] ./ ni, filter(p -> !isnothing(gi[p]), collect(ps)))
 		end
 		foreach(p -> gs[p] !== nothing && gs[p] ./= batchsize, ps)
+
 		Optimise.update!(opt, ps, gs)
 		mod(t, 1000) == 0 && @show mean(logpdf(m, x))
 	end
 end
 
-x = flower2(1000, npetals = 9)
-m = sptn(2, 9, 1)
+x = flower2(200, npetals = 9)
 
-gd!(m, x, 10000, 100)
+m1 = sptn(2, 9, 1)
+m2 = sptn(2, 9, 1)
+m3 = sptn(2, 9, 1)
+
+gd!(m1, x, 10000)
+println("done")
+gd!(m2, x, 10000, 100, normconst = :exact)
+println("done")
+gd!(m3, x, 10000, 100, normconst = :sample)
+println("done")
