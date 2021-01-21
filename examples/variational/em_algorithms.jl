1c6a6e192e91a683478f2481e885c307bb5460bb
mlnpapez <mlnpapez@gmail.com>
2021-01-21 21:36:40 +0100

    experiments: simplifying the EM algorithms

--- examples/variational/em_algorithms.jl
+++ examples/variational/em_algorithms.jl
@@ -1,106 +1,33 @@
-using SumProductTransform: TransformationNode, SVDDense, logsumexp
-using ToyProblems, Flux, Distributions, Unitary, Plots
-include("distributions.jl")
+using SumProductTransform:
+    TransformationNode,
+    SumNode,
+    SVDDense,
+	MvNormal,
+	logpdf,
+	logsumexp
+using ToyProblems:
+	flower2
+using Flux:
+    gradient,
+    params,
+	ADAM
+using Flux.Optimise:
+    update!
+using Statistics
+using StatsBase:
+    Weights,
+    sample
+using Plots
 
 plotly()
 
-
-function em!(x, c, α, niter, nepoch; opt=ADAM(), p=Flux.params(c))
-	α₀, α = deepcopy(α), deepcopy(α)
-
-	loglik = []
-
-	for t in 1:niter * nepoch
-		ρ = logcond(x, c) .+ log.(α)
-		r = normlogs(ρ)
-
-		α = α₀ + sum(r, dims = 2)[:]
-
-		g = gradient(() -> - sum(r .* logcond(x, c)), p)
-		Flux.Optimise.update!(opt, p, g)
-
-		mod(t, niter) == 0 && (ll = @show mean(log_likelihood(c, α, x)); push!(loglik, ll))
-	end
-
-	loglik
+function gmm(k)
+	SumNode([TransformationNode(SVDDense(2, identity, :butterfly), MvNormal(2, 1f0)) for _ in 1:k])
 end
 
-function sem!(x, c, α, niter, nepoch, nk, ni, ϕ; opt=ADAM(), p=Flux.params(c))
-	α = deepcopy(α)
-
-	K = length(c)
-	N = size(x, 2)
-
-	ρ = 1e-5*ones(K, N)
-
-	loglik = []
-
-	for t in 1:niter * nepoch
-		i = randorrange(N, ni)
-		k = randorrange(K, nk)
-
-		ck = c[k]
-
-		ρ[k, i] = logcond(x[:, i], ck) .+ log.(α[k])
-		r = normlogs(ρ[:, i])
-
-		α = (1-ϕ)*α + ϕ*sum(r, dims=2)[:]
-
-		g = gradient(() -> - sum(r[k, :] .* logcond(x[:, i], ck)), p)
-		Flux.Optimise.update!(opt, p, g)
-
-		mod(t, niter) == 0 && (ll = @show mean(log_likelihood(c, α, x)); push!(loglik, ll))
-	end
-
-	loglik
-end
-
-function semcv!(x, ct, α, niter, nepoch, nk, ni, ϕ; opt=ADAM(), pt=Flux.params(ct))
-	α₀, αt = deepcopy(α), deepcopy(α)
-
-	K = length(ct)
-	N = size(x, 2)
-
-	ρt = 1e-5*ones(K, N)
-
-	loglik = []
-
-	for e in 1:nepoch
-		ρe = logcond(x, ct) .+ log.(αt)
-		re = normlogs(ρe)
-
-		αe = α₀ + sum(re, dims = 2)[:]
-
-		ce = deepcopy(ct)
-		pe = Flux.params(ce)
-		ge = gradient(() -> - sum(re .* logcond(x, ce)), pe)
-
-		for t in 1:niter
-			i = randorrange(N, ni)
-			k = randorrange(K, nk)
-
-			ctk = ct[k]
-			cek = ce[k]
-
-			ρt[k, i] = logcond(x[:, i], ctk) .+ log.(αt[k])
-			rt = normlogs(ρt[:, i])
-
-			αt = (1-ϕ)*αt + ϕ*(sum(rt, dims=2)[:] - sum(re[:, i], dims=2)[:] + αe)
-
-			gti = gradient(() -> - sum(rt[k, :] .* logcond(x[:, i], ctk)), pt)
-			gei = gradient(() -> - sum(re[k, i] .* logcond(x[:, i], cek)), pe)
-
-			[gti[a] != nothing && (gti[a] .= gti[a] .- gei[b] .+ ge[b]) for (a, b) in zip(pt, pe)]
-
-	        Flux.Optimise.update!(opt, pt, gti)
-		end
-	    ll = @show mean(log_likelihood(ct, αt, x)); push!(loglik, ll)
-	end
-
-	loglik
-end
-
-logcond(x, c) = vcat(map(z -> logpdf(z, x)', c)...)
+logcond(m, x)  = vcat(map(c -> logpdf(c, x)', m.components)...)
+logjoint(m, x) = vcat(map(c -> logpdf(c, x)', m.components)...) .+ m.prior
+logjoint(m, x, z) = vcat(map(c -> logpdf(c, x)', m.components[z])...) .+ m.prior[z]
 
 function normlogs(x)
 	x = exp.(x .- maximum(x, dims=2))
@@ -115,57 +42,124 @@ function randorrange(m, n)
 	end
 end
 
-function gmm(kk)
-	tuple([TransformationNode(SVDDense(2, identity, :butterfly), MvNormal(2,1f0)) for _ in 1:kk]...)
-	# SumNode([MvNormal(2,1f0) for _ in 1:K])
+function em!(m::SumNode, x::Array{Float64, 2}, niter::Int, nepoch::Int; opt = ADAM(), ps = params(m))
+	loglik = []
+
+	for t in 1:niter * nepoch
+		ρ = logjoint(m, x)
+		r = normlogs(ρ)
+
+		gs = gradient(() -> -sum(r .* logjoint(m, x)), ps)
+		update!(opt, ps, gs)
+
+		mod(t, niter) == 0 && (ll = @show mean(logpdf(m, x)); push!(loglik, ll))
+	end
+end
+
+function sem!(m::SumNode, x::Array{Float64, 2}, niter::Int, nepoch::Int, nk::Int, ni::Int; opt = ADAM(), ps = params(m))
+	K = size(m.prior, 1)
+	N = size(x, 2)
+
+	ρ = 1e-5*ones(K, N)
+
+	loglik = []
+
+	for t in 1:niter * nepoch
+		i = randorrange(N, ni)
+		k = randorrange(K, nk)
+
+		ρ[k, i] = logjoint(m, x[:, i], k)
+		r = normlogs(ρ[:, i])
+
+		gs = gradient(() -> - sum(r[k, :] .* logjoint(m, x[:, i], k)), ps)
+		update!(opt, ps, gs)
+
+		mod(t, niter) == 0 && (ll = @show mean(logpdf(m, x)); push!(loglik, ll))
+	end
+
+	loglik
+end
+
+function semcv!(mt::SumNode, x::Array{Float64, 2}, niter::Int, nepoch::Int, nk::Int, ni::Int; opt = ADAM(), pt = params(mt))
+	K = size(mt.prior, 1)
+	N = size(x, 2)
+
+	ρt = 1e-5*ones(K, N)
+
+	loglik = []
+
+	for e in 1:nepoch
+		ρe = logjoint(mt, x)
+		re = normlogs(ρe)
+
+		me = deepcopy(mt)
+		pe = params(me)
+		ge = gradient(() -> - sum(re .* logjoint(me, x)), pe)
+
+		for t in 1:niter
+			i = randorrange(N, ni)
+			k = randorrange(K, nk)
+
+			ρt[k, i] = logjoint(mt, x[:, i], k)
+			rt = normlogs(ρt[:, i])
+
+			gti = gradient(() -> - sum(rt[k, :] .* logjoint(mt, x[:, i], k)), pt)
+			gei = gradient(() -> - sum(re[k, i] .* logjoint(me, x[:, i], k)), pe)
+
+			[gti[a] !== nothing && (gti[a] .= gti[a] .- gei[b] .+ ge[b]) for (a, b) in zip(pt, pe)]
+
+	        update!(opt, pt, gti)
+		end
+	    ll = @show mean(logpdf(mt, x)); push!(loglik, ll)
+	end
+
+	loglik
 end
 
 K = 9
 N = 200
-x = flower(Float32, N)
-α = fill(0.001f0, K)
+x = flower2(N, npetals = K)
 
 niter = 1000
 nepoch = 20
 nk = 3
 ni = 100
-ϕ = 0.005
 
-ll_bem      =    em!(x, gmm(K), α, niter, nepoch)
-ll_sem_1    =   sem!(x, gmm(K), α, niter, nepoch,  0,  0, 1.0)
-ll_sem_2    =   sem!(x, gmm(K), α, niter, nepoch,  0,  0, ϕ  )
-ll_sem_3    =   sem!(x, gmm(K), α, niter, nepoch,  0, ni, ϕ  )
-ll_sem_4    =   sem!(x, gmm(K), α, niter, nepoch, nk,  0, ϕ  )
-ll_sem_5    =   sem!(x, gmm(K), α, niter, nepoch, nk, ni, ϕ  )
-ll_sem_cv_1 = cvsem!(x, gmm(K), α, niter, nepoch,  0,  0, 1.0)
-ll_sem_cv_2 = cvsem!(x, gmm(K), α, niter, nepoch,  0,  0, ϕ  )
-ll_sem_cv_3 = cvsem!(x, gmm(K), α, niter, nepoch,  0, ni, ϕ  )
-ll_sem_cv_4 = cvsem!(x, gmm(K), α, niter, nepoch, nk,  0, ϕ  )
-ll_sem_cv_5 = cvsem!(x, gmm(K), α, niter, nepoch, nk, ni, ϕ  )
+ll_bem      =    em!(gmm(K), x, niter, nepoch)
+ll_sem_1    =   sem!(gmm(K), x, niter, nepoch,  0,  0) # ϕ=1.0
+# ll_sem_2    =   sem!(x, gmm(K), α, niter, nepoch,  0,  0, ϕ  )
+# ll_sem_3    =   sem!(x, gmm(K), α, niter, nepoch,  0, ni, ϕ  )
+# ll_sem_4    =   sem!(x, gmm(K), α, niter, nepoch, nk,  0, ϕ  )
+# ll_sem_5    =   sem!(x, gmm(K), α, niter, nepoch, nk, ni, ϕ  )
+ll_sem_cv_1 = semcv!(gmm(K), x, niter, nepoch,  0,  0) # ϕ=1.0
+# ll_sem_cv_2 = semcv!(x, gmm(K), α, niter, nepoch,  0,  0, ϕ  )
+# ll_sem_cv_3 = semcv!(x, gmm(K), α, niter, nepoch,  0, ni, ϕ  )
+# ll_sem_cv_4 = semcv!(x, gmm(K), α, niter, nepoch, nk,  0, ϕ  )
+# ll_sem_cv_5 = semcv!(x, gmm(K), α, niter, nepoch, nk, ni, ϕ  )
 
-p1 = plot(
-    hcat(ll_bem, ll_sem_1, ll_sem_2, ll_sem_3, ll_sem_4, ll_sem_5),
-    label=["bem" "sem-sanity check" "sem-none" "sem-x" "sem-z" "sem-xz"],
-    markershape=[:circle :xcross :cross :circle :circle :circle :circle],
-    xlabel="epoch [-]",
-    ylabel="log-likelihood [-]",
-    title="sEM",
-    foreground_color_legend = nothing,
-    background_color_legend = nothing,
-    legend=:bottomright,
-    )
+# p1 = plot(
+#     hcat(ll_bem, ll_sem_1, ll_sem_2, ll_sem_3, ll_sem_4, ll_sem_5),
+#     label=["bem" "sem-sanity check" "sem-none" "sem-x" "sem-z" "sem-xz"],
+#     markershape=[:circle :xcross :cross :circle :circle :circle :circle],
+#     xlabel="epoch [-]",
+#     ylabel="log-likelihood [-]",
+#     title="sEM",
+#     foreground_color_legend = nothing,
+#     background_color_legend = nothing,
+#     legend=:bottomright,
+#     )
 
-p2 = plot(
-    hcat(ll_bem, ll_sem_cv_1, ll_sem_cv_2, ll_sem_cv_3, ll_sem_cv_4, ll_sem_cv_5),
-    label=["bem" "sem_cv-sanity check" "sem_cv-none" "sem_cv-x" "sem_cv-z" "sem_cv-xz"],
-    markershape=[:circle :xcross :cross :circle :circle :circle],
-    xlabel="epoch [-]",
-    ylabel="log-likelihood [-]",
-    title="sEM control variates",
-    foreground_color_legend = nothing,
-    background_color_legend = nothing,
-    legend=:bottomright,
-    )
+# p2 = plot(
+#     hcat(ll_bem, ll_sem_cv_1, ll_sem_cv_2, ll_sem_cv_3, ll_sem_cv_4, ll_sem_cv_5),
+#     label=["bem" "sem_cv-sanity check" "sem_cv-none" "sem_cv-x" "sem_cv-z" "sem_cv-xz"],
+#     markershape=[:circle :xcross :cross :circle :circle :circle],
+#     xlabel="epoch [-]",
+#     ylabel="log-likelihood [-]",
+#     title="sEM control variates",
+#     foreground_color_legend = nothing,
+#     background_color_legend = nothing,
+#     legend=:bottomright,
+#     )
 
-l = @layout [a b]
-plot(p1, p2, layout = l)
+# l = @layout [a b]
+# plot(p1, p2, layout = l)
