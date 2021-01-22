b186a6937c4a60585ed0e1ddb1a3bce4840b5de3
Pevnak <pevnak@gmail.com>
2021-01-22 11:45:13 +0100

    polishing MetropolisHastings

--- examples/variational/metropolis_hastings.jl
+++ examples/variational/metropolis_hastings.jl
@@ -4,13 +4,15 @@ using SumProductTransform:
     SVDDense,
 	MvNormal,
 	logpdf,
-	logsumexp
+	logsumexp,
+	updatepriors!
 using ToyProblems:
 	flower2
 using Flux:
     gradient,
     params,
-	ADAM
+	ADAM,
+	onehotbatch
 using Flux.Optimise:
     update!
 using Statistics
@@ -84,55 +86,64 @@ function a3!(m::SumNode, x::Array{Float64, 2}, nstep::Int; nsamp::Int = 10, opt 
 	end
 end
 
+function mhsample(components, q, x, pₒ, kₒ)
+	kₙ = sample(Weights(q))
+	pₙ = logpdf(components[kₙ], x)
+	qₒ, qₙ = q[kₒ], q[kₙ]
+	accept = log(rand()) < pₙ - pₒ + qₒ - qₙ
+	accept ? (pₙ, kₙ) : (pₒ, kₒ)
+end
+
+function mhsampler(components, q, x, k, n)
+	p = logpdf(components[k], x)
+	s = zeros(Int, length(q))
+	for i in 1:n
+		p, k = mhsample(components, q, x, p, k)
+		s[k] += 1
+	end
+	(p, k, s)
+end
+
+using PrayTools
+
+function ∇tlogpdf(components, x, k, ps)
+	ki = PrayTools.classindexes(k)
+	samples = [(components[j], x[:, jj]) for (j,jj) in ki]
+	PrayTools._pgradient((c, x) -> -sum(logpdf(c, x)), ps, samples)
+end
+
 function a4!(m::SumNode, x::Array{Float64, 2}, nstep::Int; nsamp::Int = 10, opt = ADAM(), ps = params(m), ϕ::Float64 = 0.001)
 	ndata = size(x, 2)
 	ncomp = length(m.components)
-
 	table = ones(ncomp, ndata)
+	k = rand(1:ncomp, ndata)
 
 	for t in 1:nstep
 		q = normtable(table)
-		z_prev = rand(1:ncomp)
+		ii = rand(1:ndata, 100)
+		s = zeros(Int8, ncomp, length(ii))
 
-		s = zeros(Int8, ncomp, ndata)
-		k = zeros(Int8, nsamp, ndata)
-		for i in 1:ndata
-			p_prev = logpdf(m.components[z_prev], x[:, i]) + m.prior[z_prev]
-
-			for j in 1:nsamp
-				z_curr = sample(Weights(q[:, i]))
-				p_curr = logpdf(m.components[z_curr], x[:, i]) + m.prior[z_curr]
-				q_curr = log(q[z_curr, i])
-				q_prev = log(q[z_prev, i])
-
-				if log(rand()) < p_curr - p_prev + q_prev - q_curr
-					p_prev = p_curr
-					z_prev = z_curr
-
-					s[z_prev, i] += 1
-
-					k[j, i] = z_curr
-				else
-					k[j, i] = z_prev
-				end
-			end
+		for (j,i) in enumerate(ii)
+			_, k[i], s[:, j] = mhsampler(m.components, q[:, i], x[:,i], k[i], nsamp)
 		end
 
-		table = (1-ϕ)*table + ϕ*s
-
-		gs = gradient(() -> -sum(mean(hcat([logjoint(m, x[:, i], k[:, i]) for i in 1:ndata]...), dims=1)), ps)
+		table[:,ii] = (1-ϕ)*table[:,ii] + ϕ*s
+		# gs = gradient(() -> -sum(mean(hcat([logjoint(m, x[:, i], z[:, i]) for i in 1:ndata]...), dims=1)), ps)
+		# gs = gradient(() -> -sum(logpdf(m.components[k[i]], x[:, i]) for i in 1:ndata), ps)
+		lp, gs = ∇tlogpdf(m.components, x[:,ii], k[ii], ps)
+		
 		update!(opt, ps, gs)
-
+		updatepriors!(m, x)
 		mod(t, 1000) == 0 && @show mean(logpdf(m, x))
 	end
 end
 
 K = 9
-N = 100
+N = 10000
 x = flower2(N, npetals = K)
 niter = 100000
 
-# gd!(gmm(K), x, niter)
+gd!(gmm(K), x, niter)
 # println("__________")
 # em!(gmm(K), x, niter)
 # println("__________")
@@ -140,5 +151,6 @@ niter = 100000
 # println("__________")
 # a3!(gmm(K), x, niter)
 # println("__________")
-a4!(gmm(K), x, niter, nsamp=4)
+# m = gmm(K)
+a4!(gmm(K)	, x, niter, nsamp=4)
 println("__________")
