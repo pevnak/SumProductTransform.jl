30aaa14bb6bc58df6db41e3bb2a766af105d0a79
mlnpapez <mlnpapez@gmail.com>
2021-01-25 20:16:31 +0100

    experiments: comparing MC and MCMC score function estimators

--- examples/variational/metropolis_hastings.jl
+++ examples/variational/metropolis_hastings.jl
@@ -40,6 +40,30 @@ function normtable(x)
 	r = x ./ sum(x, dims = 1)
 end
 
+function ∇logpdf(m, x, k, ps)
+	ki = classindexes(k)
+	samples = [(m.components[j], x[:, jj], m.prior[j]) for (j, jj) in ki]
+	_pgradient((c, x, p) -> -sum(logpdf(c, x) .+ p), ps, samples)
+end
+
+function mhsample(m, q, x, pₒ, kₒ)
+	kₙ = sample(Weights(q))
+	pₙ = logpdf(m.components[kₙ], x) + m.prior[kₙ]
+	qₒ, qₙ = q[kₒ], q[kₙ]
+	accept = log(rand()) < pₙ - pₒ + qₒ - qₙ
+	accept ? (pₙ, kₙ) : (pₒ, kₒ)
+end
+
+function mhsampler(m, q, x, k, n)
+	p = logpdf(m.components[k], x) + m.prior[k]
+	s = zeros(Int, length(q))
+	for i in 1:n
+		p, k = mhsample(m, q, x, p, k)
+		s[k] += 1
+	end
+	(k, s)
+end
+
 function gd!(m::SumNode, x::Array{Float64, 2}, nstep::Int, opt = ADAM(), ps = params(m))
     for t in 1:nstep
         gs = gradient(() -> -mean(logpdf(m, x)), ps)
@@ -61,59 +85,21 @@ function em!(m::SumNode, x::Array{Float64, 2}, nstep::Int, opt = ADAM(), ps = pa
 	end
 end
 
-function a2!(m::SumNode, x::Array{Float64, 2}, nstep::Int; nsamp::Int = 10, opt = ADAM(), ps = params(m))
+function a1!(m::SumNode, x::Array{Float64, 2}, nstep::Int; opt = ADAM(), ps = params(m))
 	for t in 1:nstep
 		ρ = logjoint(m, x)
 		r = normlogs(ρ)
 
-		k = hcat([[sample(Weights(r[:, i])) for _ in 1:nsamp] for i in 1:size(x, 2)]...)
+		k = [sample(Weights(r[:, i])) for i in 1:size(x, 2)]
 
-		gs = gradient(() -> -sum(mean(hcat([logjoint(m, x[:, i], k[:, i]) for i in 1:size(x, 2)]...), dims=1)), ps)
+		_, gs = ∇logpdf(m, x, k, ps)
 		update!(opt, ps, gs)
 
 		mod(t, 1000) == 0 && @show mean(logpdf(m, x))
 	end
 end
 
-function a3!(m::SumNode, x::Array{Float64, 2}, nstep::Int; nsamp::Int = 10, opt = ADAM(), ps = params(m))
-	for t in 1:nstep
-		ρ = sum(logcond(m, x), dims=2) + m.prior
-		r = normlogs(ρ)
-
-		k = [sample(Weights(r[:])) for _ in 1:nsamp]
-
-		gs = gradient(() -> -sum(mean(hcat([logjoint(m, x[:, i], k[:]) for i in 1:size(x, 2)]...), dims=1)), ps)
-		update!(opt, ps, gs)
-
-		mod(t, 1000) == 0 && @show mean(logpdf(m, x))
-	end
-end
-
-function mhsample(m, q, x, pₒ, kₒ)
-	kₙ = sample(Weights(q))
-	pₙ = logpdf(m.components[kₙ], x) + m.prior[kₙ]
-	qₒ, qₙ = q[kₒ], q[kₙ]
-	accept = log(rand()) < pₙ - pₒ + qₒ - qₙ
-	accept ? (pₙ, kₙ) : (pₒ, kₒ)
-end
-
-function mhsampler(m, q, x, k, n)
-	p = logpdf(m.components[k], x) + m.prior[k]
-	s = zeros(Int, length(q))
-	for i in 1:n
-		p, k = mhsample(m, q, x, p, k)
-		s[k] += 1
-	end
-	(k, s)
-end
-
-function ∇logpdf(m, x, k, ps)
-	ki = classindexes(k)
-	samples = [(m.components[j], x[:, jj], m.prior[j]) for (j, jj) in ki]
-	_pgradient((c, x, p) -> -sum(logpdf(c, x) .+ p), ps, samples)
-end
-
-function a4!(m::SumNode, x::Array{Float64, 2}, nstep::Int; nsamp::Int = 10, opt = ADAM(), ps = params(m), ϕ::Float64 = 0.001)
+function a2!(m::SumNode, x::Array{Float64, 2}, nstep::Int; nsamp::Int = 10, opt = ADAM(), ps = params(m), ϕ::Float64 = 0.001)
 	ndata = size(x, 2)
 	ncomp = length(m.components)
 	table = ones(ncomp, ndata)
@@ -141,14 +127,11 @@ N = 100
 x = flower2(N, npetals = K)
 niter = 20000
 
-# gd!(gmm(K), x, niter)
-# println("__________")
-# em!(gmm(K), x, niter)
-# println("__________")
-# a2!(gmm(K), x, niter, nsamp=10)
-# println("__________")
-# a3!(gmm(K), x, niter)
-# println("__________")
-# m = gmm(K)
-a4!(gmm(K), x, niter, nsamp=1)
+gd!(gmm(K), x, niter)
+println("__________")
+em!(gmm(K), x, niter)
+println("__________")
+a1!(gmm(K), x, niter)
+println("__________")
+a2!(gmm(K), x, niter, nsamp=1)
 println("__________")
