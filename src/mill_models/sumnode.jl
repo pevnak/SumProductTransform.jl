dc9144067032551b6d6621d4ee1f5437945dbc8c
Martin Rektoris <martin.rektoris@gmail.com>
2022-09-07 19:22:19 +0200

    SumNode accepts BagNode

--- src/mill_models/sumnode.jl
+++ src/mill_models/sumnode.jl
@@ -1,5 +1,12 @@
 
 Distributions.logpdf(m::SumNode, x::ArrayNode) = logpdf(m, x.data)
-Distributions.logpdf(m::SumNode, x::BagNode) = logpdf(m, x)
+
+function Distributions.logpdf(m::SumNode, x::BagNode)
+	lkl = transpose(hcat(map(c -> logpdf(c, x) ,m.components)...))
+	w = m.prior .- logsumexp(m.prior)
+	logsumexp(w .+ lkl, dims = 1)[:]
+end
+
+Base.rand(m::SumNode, n::Integer) = rand(m.components[sample(Weights(m.prior))], n)
 
 # TO DO: Distributions.logpdf(m::SumNode, x::ProductNode) 
